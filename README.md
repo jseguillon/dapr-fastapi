c2b38af2ab89c1e658447d69752eb95fa4b1d64d
Joël Séguillon <joel.seguillon@gmail.com>
2025-01-26 22:20:54 +0100

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+# grafana and prometheus
+docker run \
+  --net=host \
+  -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
+  prom/prometheus --config.file=/etc/prometheus/prometheus.yml --web.listen-address=:8082
+
+docker run -d --net=host --name=grafana grafana/grafana
+
+# init
+
+dapr init
+
+# run
+
+```shell
+python3 -m venv ping/.venv
+source ping/.venv/bin/activate
+python3 -m pip install -r ping/requirements.txt
+```
+
+```shell
+dapr run --metrics-port 45267  --app-id ping python3 ping/app.py --app-port 8001 -- uvicorn --host 0.0.0.0 --port 8001
+```
+
+```shell
+python3 -m venv pong/.venv
+source pong/.venv/bin/activate
+python3 -m pip install -r pong/requirements.txt
+```
+
+```shell
+dapr run --metrics-port 45268  --app-id pong python3 pong/app.py --app-port 8002 -- uvicorn --host 0.0.0.0 --port 8002
+```
+
+# test
+
+```shell
+dapr invoke --app-id ping --method ping --data '{"say":"hello from cli"}'
+```
+
+```shell
+dapr publish --publish-app-id pong --topic pong --pubsub pubsub --data '{"say": "hello"}'
+```
---
+++ ping/app.py
@@ -0,0 +1,71 @@
+import argparse
+import json
+import uvicorn
+from fastapi import Body, FastAPI
+from dapr.ext.fastapi import DaprApp
+from pydantic import BaseModel
+from dapr.clients import DaprClient
+
+class PingPong(BaseModel):
+    say: str
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "say": "Hello from ping",
+                }
+            ]
+        }
+    }
+
+class CloudEventModel(BaseModel):
+    data: PingPong
+    datacontenttype: str
+    id: str
+    pubsubname: str
+    source: str
+    specversion: str
+    topic: str
+    traceid: str
+    traceparent: str
+    tracestate: str
+    type: str
+
+app = FastAPI()
+dapr_app = DaprApp(app)
+c = DaprClient()
+
+# Wait pong topic event
+@dapr_app.subscribe(pubsub='pubsub', topic='pong')
+def cloud_event_handler(event_data: CloudEventModel):
+        print(f"Received cloud event on ping topic: {event_data}")
+
+# In case of POST /ping: emit event on ping topic
+# dapr invoke --app-id ping --method ping --data '{"say":"hello from cli"}' 
+@app.post("/ping", summary="Ping endpoint"  )
+def get_my_data(message: PingPong):
+    print(f"received POST ping with message: {message}")
+    print(f"Submitting pong event")
+    event_data_dict = {
+        'say': f'hello world from ping: {message.say}'
+    }
+    c.publish_event(
+        pubsub_name="pubsub",
+        topic_name="ping",
+        data=json.dumps(event_data_dict),
+        data_content_type="application/json"
+    )
+
+@app.get("/healthz")
+async def healthcheck():
+    return "Healthy!"
+
+# Argument parsing
+import argparse
+parser = argparse.ArgumentParser(description="Run the FastAPI app with Uvicorn.")
+parser.add_argument("--port", type=int, default=8000, help="Port to run the Uvicorn server on.")
+parser.add_argument("--log_level, -l", type=str, default="trace", help="Log level for Uvicorn.")
+args, unknown = parser.parse_known_args()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=args.port, log_level="trace")
